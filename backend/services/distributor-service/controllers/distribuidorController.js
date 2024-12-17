d2d2a28cca3d87095bc3ffb1923784aeaf176fd5
David <pirayguaman@gmail.com>
2024-12-17 07:24:22 -0500

    Actualización de validaciones v1.1

--- backend/services/distributor-service/controllers/distribuidorController.js
+++ backend/services/distributor-service/controllers/distribuidorController.js
@@ -1,21 +1,27 @@
 const { createDistributorSchema, updateDistributorSchema } = require('../validations/distribuidorValidation');
 const Distributor = require('../models/distribuidorModel');
+const { exist } = require('joi');
 
 const DistributorController = {
+  // Crear distribuidor
   async create(req, res) {
+    // Validar con el esquema
     const { error } = createDistributorSchema.validate(req.body);
     if (error) {
       return res.status(400).json({ error: error.details[0].message });
     }
-
     try {
       const response = await Distributor.createDistributor(req.body);
+      if (response.error){
+        return res.status(404).json({error: response.error});    
+      }
       res.status(201).json(response);
     } catch (error) {
       res.status(500).json({ error: 'Fallo al crear un distribuidor' });
     }
   },
 
+  // Obtener todos los distribuidores
   async getAll(req, res) {
     try {
       const distributors = await Distributor.getAllDistributors();
@@ -25,6 +31,7 @@ const DistributorController = {
     }
   },
 
+  // Obtener un distribuidor por ID
   async getById(req, res) {
     try {
       const distributor = await Distributor.getDistributorById(req.params.id);
@@ -37,6 +44,7 @@ const DistributorController = {
     }
   },
 
+  // Actualizar la información de un distribuidor
   async update(req, res) {
     const { error } = updateDistributorSchema.validate(req.body);
     if (error) {
@@ -51,6 +59,7 @@ const DistributorController = {
     }
   },
 
+  // Eliminar un distribuidor
   async delete(req, res) {
     try {
       const response = await Distributor.deleteDistributor(req.params.id);
@@ -63,6 +72,7 @@ const DistributorController = {
     }
   },
 
+  // Obtener una zona por distribuidor
   async getByZone(req, res) {
     try {
       const distributors = await Distributor.getDistributorsByZone(req.params.zoneID);
--- backend/services/distributor-service/models/distribuidorModel.js
+++ backend/services/distributor-service/models/distribuidorModel.js
@@ -1,29 +1,48 @@
 const db = require('../../../utils/firebase');
 
 const Distributor = {
+  // Verificar si ya existe un ID repetido
+  async existsById(id_distribuidor) {
+    const doc = await db.collection('distribuidor').doc(id_distribuidor).get();
+    return doc.exists;
+  },
+
+  // Crear un distribuidor
   async createDistributor(distributorData) {
-    const distributorRef = db.collection('distribuidor').doc(distributorData.id_distribuidor);
+    const { id_distribuidor } = distributorData;
+    // Verificar duplicidad en ID
+    const exists = await this.existsById(id_distribuidor);
+    // console.log(exists);
+    const distributorRef = db.collection('distribuidor').doc(id_distribuidor);
+    if (exists) {
+      return { error: 'Distribuidor con ID repetido' };
+    }
+    // Crear el documento con ID manual
     await distributorRef.set(distributorData);
     return { message: 'Distribuidor creado con éxito' };
   },
 
+  // Obtener distribuidor por ID
   async getDistributorById(id_distribuidor) {
     const distributorRef = db.collection('distribuidor').doc(id_distribuidor);
     const distributorDoc = await distributorRef.get();
     return distributorDoc.exists ? distributorDoc.data() : null;
   },
 
+  // Obtener todos los distribuidores
   async getAllDistributors() {
     const snapshot = await db.collection('distribuidor').get();
     return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
   },
 
+  // Actualizar la información de un distribuidor
   async updateDistributor(id_distribuidor, updatedData) {
     const distributorRef = db.collection('distribuidor').doc(id_distribuidor);
     await distributorRef.update(updatedData);
     return { message: 'Distribuidor actualizado con éxito' };
   },
 
+  // Eliminar un distribuidor
   async deleteDistributor(id_distribuidor) {
     const distributorRef = db.collection('distribuidor').doc(id_distribuidor);
     const distributorDoc = await distributorRef.get();
@@ -34,6 +53,7 @@ const Distributor = {
     return { message: 'Distribuidor eliminado con éxito' };
   },
 
+  // Obtener una zona por Distribuidor
   async getDistributorsByZone(zoneID) {
     const snapshot = await db.collection('distribuidor').where('zonaAsignada', '==', zoneID).get();
     return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
