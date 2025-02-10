f3c1ebac0cefa687c053538615ae6315c99a15b4
Kevi23777 <kevinguapi@gmail.com>
2025-02-10 17:40:47 -0500

    Añade ayudas a las pantallas

--- frontend/lib/app_routes.dart
+++ frontend/lib/app_routes.dart
@@ -7,12 +7,14 @@ import 'screens/client/client_screen.dart';
 import 'screens/client/registration_screen.dart';
 import 'screens/client/profile_screen.dart';
 import 'screens/client/orders_history_screen.dart';
+import 'screens/client/help_screen.dart';
 // Para distribuidores
 import 'screens/distributor/distributor_screen.dart';
 import 'screens/distributor/inventory_screen.dart';
 import 'screens/distributor/profile_screen.dart';
 import 'screens/distributor/report_distributor_screen.dart';
 import 'screens/distributor/settings_distributor_screen.dart';
+import 'screens/distributor/help_screen.dart';
 // Para Gerente
 import 'screens/gerente/gerente_screen.dart';
 import 'screens/gerente/listdistribuidor_screen.dart';
@@ -20,6 +22,7 @@ import 'screens/gerente/listproductos_screen.dart';
 import 'screens/gerente/reports_screen.dart';
 import 'screens/gerente/map_gerente.dart';
 import 'screens/gerente/settings_gerente_screen.dart';
+import 'screens/gerente/help_screen.dart';
 // Extras
 import 'screens/utils/map_screen.dart';
 import 'screens/home/main_screen.dart';
@@ -35,19 +38,23 @@ class AppRoutes {
     '/cliente': (context) => const ClientScreen(),
     '/perfil-cliente': (context) => const ProfileClientScreen(),
     '/historial-cliente': (context) => const OrdersClientScreen(),
+    '/ayuda-cliente': (context) => AyudaClienteScreen(),
 
     // Distribuidor
     '/distribuidor': (context) => const DistributorScreen(),
     '/inventario-distribuidor': (context) => const InventoryScreen(),
     '/perfil-distribuidor': (context) => const ProfileDistributorScreen(),
     '/reporte-distribuidor': (context) => const DistributorDashboard(),
-    '/configuracion-distribuidor': (context) => const DistributorSettingsScreen(),
+    '/configuracion-distribuidor': (context) =>
+        const DistributorSettingsScreen(),
+    '/ayuda-distribuidor': (context) => AyudaDistribuidorScreen(),
 
     // Gerente
     '/gerente': (context) => DirectorScreen(),
     '/lista-distribuidores': (context) => ListdistribuidorScreen(),
     '/reporte-gerente': (context) => const ReportsScreen(),
     '/mapa-gerente': (context) => const ManagerMapScreen(),
+    '/ayuda-gerente': (context) => AyudaGerenteScreen(),
     '/configuracion-gerente': (context) => const ManagerSettingsScreen(),
 
     // Extras
--- frontend/lib/core/constants/menu_items.dart
+++ frontend/lib/core/constants/menu_items.dart
@@ -6,34 +6,76 @@ class MenuItems {
       {'title': 'Dashboard', 'icon': Icons.dashboard, 'route': '/dashboard'},
       {'title': 'Usuarios', 'icon': Icons.people, 'route': '/usuarios'},
       {'title': 'Pedidos', 'icon': Icons.list, 'route': '/pedidos'},
-      {'title': 'Productos', 'icon': Icons.shopping_cart, 'route': '/productos'},
+      {
+        'title': 'Productos',
+        'icon': Icons.shopping_cart,
+        'route': '/productos'
+      },
       {'title': 'Salir', 'icon': Icons.logout, 'route': '/logout'},
     ],
     'cliente': [
       {'title': 'Inicio', 'icon': Icons.home, 'route': '/cliente'},
       {'title': 'Perfil', 'icon': Icons.person, 'route': '/perfil-cliente'},
-      {'title': 'Historial', 'icon': Icons.history, 'route': '/historial-cliente'},
+      {
+        'title': 'Historial',
+        'icon': Icons.history,
+        'route': '/historial-cliente'
+      },
       {'title': 'Pedidos', 'icon': Icons.list, 'route': '/pedidos-cliente'},
       {'title': 'Ayuda', 'icon': Icons.help, 'route': '/ayuda-cliente'},
       {'title': 'Salir', 'icon': Icons.logout, 'route': '/logout'},
     ],
     'distribuidor': [
       {'title': 'Inicio', 'icon': Icons.home, 'route': '/distribuidor'},
-      {'title': 'Perfil', 'icon': Icons.person, 'route': '/perfil-distribuidor'},
-      {'title': 'Inventario', 'icon': Icons.water_drop, 'route': '/inventario-distribuidor'},
-      {'title': 'Reportes', 'icon': Icons.description, 'route': '/reporte-distribuidor'},
-      {'title': 'Configuraciones', 'icon': Icons.settings, 'route': '/configuracion-distribuidor'},
+      {
+        'title': 'Perfil',
+        'icon': Icons.person,
+        'route': '/perfil-distribuidor'
+      },
+      {
+        'title': 'Inventario',
+        'icon': Icons.water_drop,
+        'route': '/inventario-distribuidor'
+      },
+      {
+        'title': 'Reportes',
+        'icon': Icons.description,
+        'route': '/reporte-distribuidor'
+      },
+      {
+        'title': 'Configuraciones',
+        'icon': Icons.settings,
+        'route': '/configuracion-distribuidor'
+      },
+      {'title': 'Ayuda', 'icon': Icons.help, 'route': '/ayuda-distribuidor'},
       {'title': 'Salir', 'icon': Icons.logout, 'route': '/logout'},
     ],
     'gerente': [
       {'title': 'Menu', 'icon': Icons.home, 'route': '/gerente'},
       {'title': 'Perfil', 'icon': Icons.person, 'route': '/perfil-gerente'},
-      {'title': 'Distribuidores', 'icon': Icons.local_shipping, 'route': '/lista-distribuidores'},
+      {
+        'title': 'Distribuidores',
+        'icon': Icons.local_shipping,
+        'route': '/lista-distribuidores'
+      },
       {'title': 'Mapa', 'icon': Icons.map, 'route': '/mapa-gerente'},
       {'title': 'Clientes', 'icon': Icons.groups, 'route': '/lista-clientes'},
-      {'title': 'Productos', 'icon': Icons.shopping_cart, 'route': '/lista-productos'},
-      {'title': 'Reportes', 'icon': Icons.description, 'route': '/reporte-gerente'},
-      {'title': 'Configuraciones', 'icon': Icons.settings, 'route': '/configuracion-gerente'},
+      {
+        'title': 'Productos',
+        'icon': Icons.shopping_cart,
+        'route': '/lista-productos'
+      },
+      {
+        'title': 'Reportes',
+        'icon': Icons.description,
+        'route': '/reporte-gerente'
+      },
+      {
+        'title': 'Configuraciones',
+        'icon': Icons.settings,
+        'route': '/configuracion-gerente'
+      },
+      {'title': 'Ayuda', 'icon': Icons.help, 'route': '/ayuda-gerente'},
       {'title': 'Salir', 'icon': Icons.logout, 'route': '/logout'},
     ],
   };
